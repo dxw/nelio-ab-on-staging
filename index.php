be45a0f4b2dcc7fb9a7675d304b08f35fed81288
Rob Skilling <rob@dxw.com>
2025-02-28 08:05:12 +0000

    Scaffold main plugin file

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * WordPress Plugin
+ *
+ * @package     WordPressPlugin
+ * @author      dxw
+ * @copyright   2020
+ * @license     MIT
+ *
+ * @wordpress-plugin
+ * Plugin Name: Nelio AB On Staging
+ * Plugin URI: https://github.com/dxw/nelio-ab-on-staging
+ * Description: Enable full Nelio AB Testing functionality on staging environments. Not required on prod.
+ * Author: dxw
+ * Version: 1.0.0
+ */
